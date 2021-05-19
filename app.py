c5c656a9d3ed9a3f59b22da312a06aa066592483
Pavel Marusov <dreik7570000@gmail.com>
2021-05-20 00:24:45 +0300

    GUI (add list item && event click button)

---
+++ app.py
@@ -0,0 +1,46 @@
+import sys
+import json
+from datetime import datetime
+from PyQt5 import QtWidgets, uic
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('./view/main.ui', self)
+
+        self.pushButton.setText("Text changed")
+        self.pushButton.clicked.connect(self.printButtonPressed)
+
+        self.show()
+
+    def printButtonPressed(self):
+        # This is executed when the button is pressed
+        self.listWidget.addItem('asd')
+        print('printButtonPressed')
+
+# GUI Variables #
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
+
+
+# Json format variables #
+# now = datetime.now()
+# history = []
+# record = {"message": "Lol you father", "createAt": now.strftime("%d.%m.%Y %H:%M")}
+
+# history.append(record)
+
+# try:
+#     with open('./data.json', 'r+') as f:
+#         data = json.load(f)
+#         data.append(record)
+#         f.seek(0)
+#         json.dump(data, f, ensure_ascii=False, indent=4)
+#         print(data)
+# except IOError:
+#     with open("./data.json", "w", encoding='utf-8') as outfile:
+#         json.dump(history, outfile, ensure_ascii=False, indent=4)
+#     print("Файл не найден!")
+
+# print(record)
